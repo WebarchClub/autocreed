68751b153a4668fde2b3b3a40ac640d47474f986
Yadvi Bhalla <60046892+yadvi12@users.noreply.github.com>
2020-07-12 23:30:51 +0530

    Added styling to the sponsors section

--- public/js/home.js
+++ public/js/home.js
@@ -1,4 +1,1 @@
-// sponsors
-$('.carousel').carousel({
-    interval: 2000
-});
+.
--- public/js/nav.js
+++ public/js/nav.js
@@ -1,1 +1,17 @@
-.
+$(document).click(function(e) {
+	if (!$(e.target).is('.navbar-collapse')) {
+        $('.collapse').collapse('hide');
+        // $('.icon').toggleClass('newLocation');	    
+    }
+});
+
+/* navbar bg changed after scrolling 50px */
+$(window).scroll(function() {
+    
+    if ($(document).scrollTop() > 50) {
+      $('.navbar').addClass('blurry');
+    } else {
+      $('.navbar').removeClass('blurry');
+
+    }
+  });
