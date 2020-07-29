c10794838c719d174944f91df5ef8d7bc6dca440
Mohammad MuaZam <mm9084@srmist.edu.in>
2020-07-29 09:45:10 +0530

    changed

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,17 +1,33 @@
-$(document).click(function(e) {
-	if (!$(e.target).is('.navbar-collapse')) {
-        $('.collapse').collapse('hide');
-        // $('.icon').toggleClass('newLocation');	    
-    }
-});
-$(document).ready(function(){
 
+$(document).ready(function(){
+  // setTimeout(function(){
+  //   $('.navbar-brand').toggleClass('disappear');
+  //   }, 6000);
   $("button").click(function(){
   
     $('.navbar-toggler').toggleClass('shift');
+    // $('.navbar-brand').toggleClass('disappear');
+    
   });
 });
-/* navbar bg changed after scrolling 50px */
+
+$("button").click(function () {
+  var el = $('.navbar-brand');
+  window.setTimeout(function() {
+      el.toggleClass('disappear');
+  }, 300);
+});
+
+// $(document).click(function(e) {
+// 	if (!$(e.target).is('.navbar-collapse')) {
+//         $('.collapse').collapse('hide');
+//         $(".navbar-toggler").addClass('shift');
+
+//         $(".navbar-brand").removeClass('disappear');
+//         // $('.icon').toggleClass('newLocation');	    
+//     }
+// });
+//  navbar bg changed after scrolling 50px
 $(window).scroll(function() {
     
     if ($(document).scrollTop() > 50) {
