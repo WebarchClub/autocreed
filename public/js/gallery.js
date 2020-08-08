3379faa1c6d9d516f7662b664c0674960f62dd01
Yadvi Bhalla <60046892+yadvi12@users.noreply.github.com>
2020-08-08 17:39:38 +0530

    Add files via upload

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -1,7 +1,1 @@
-$(document).ready(function () {
-    $(".gallery-img").click(function () {
-        var t = $(this).attr("src");
-        $(".modal-body").html("<img src='" + t + "' class='modal-img'>");
-        $("#myModal").modal();
-    });
-});
+.
