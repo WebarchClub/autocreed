dc7f89044a64dd8e01a358b25b043d29b76ce088
Raghu Raj <raghu-raj23@users.noreply.github.com>
2020-09-26 21:05:44 +0530

    added carousel controls

--- public/js/home.js
+++ public/js/home.js
@@ -2,7 +2,7 @@
 var textWrapper = document.querySelector('.ml11 .letters');
 textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
 
-anime.timeline({loop: true})
+anime.timeline({loop: false})
   .add({
     targets: '.ml11 .line',
     scaleY: [0,1],
@@ -19,16 +19,15 @@ anime.timeline({loop: true})
   }).add({
     targets: '.ml11 .letter',
     opacity: [0,1],
-    easing: "easeOutExpo",
-    duration: 600,
+    easing: "easeOutSine",
+    duration: 700,
     offset: '-=775',
     delay: (el, i) => 34 * (i+1)
   }).add({
     targets: '.ml11',
-    opacity: 0,
-    duration: 1000,
+    duration: 500,
     easing: "easeOutExpo",
-    delay: 1000
+    delay: 300
   });
 /*================ Carousel=================*/
 
